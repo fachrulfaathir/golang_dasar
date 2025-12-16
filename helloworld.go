88e8e4507f4d149ecfa70882ae1467a2df39e7c2
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2025-12-16 12:00:17 +0700

    update varargs func

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func sumAll(numbers ...int) int {
+    total := 0
+    for _, number := range numbers {
+        total += number
+    }
+    return total;
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    slice := []int{10,2,2,22};
+
+    // sumAll := sumAll(10,5,6,7,4)
+
+    sumAllSlice := sumAll(slice...)
+
+    fmt.Println(sumAllSlice)
+    // fmt.Println("sum nya : ", sumAll)
+
+    // s := []int{}
+
+    // person := map[string]string{
+    //     "name" : "fachrul",
+    //     "address" : "jl bba cijeruk",
+    // }
+
+    // person["phone"] = "085171187250"
+
+    // fmt.Println(person["name"]);
+
+
+    // for k,v := range person{
+    //     fmt.Println(k," : ",v)
+    // }
+
+
+    // s= append(s, 1,2,3,4)
+    // s= slices.Delete(s,0,2)
+    
+    // fmt.Println(s)
 }
